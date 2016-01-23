5dfb9a6f0854ae2cdf7be61fade11d13e44602cb
Sandeep Deb <deb.sandeep@gmail.com>
2016-01-23 16:26:26 +0530

    StatusBar adjustments

--- src/main/java/com/sandy/common/ui/statusbar/StatusBar.java
+++ src/main/java/com/sandy/common/ui/statusbar/StatusBar.java
@@ -93,7 +93,7 @@ public class StatusBar extends JPanel implements Initializable {
             components = this.eastPanelComponents ;
         }
         
-        panel.setLayout( new FlowLayout( layoutDir ) );
+        panel.setLayout( new FlowLayout( layoutDir, 0, 0 ) );
         for( int i=0; i<components.size(); i++ ) {
             panel.add( components.get( i ) ) ;
             if( i < (components.size()-1) ) {
