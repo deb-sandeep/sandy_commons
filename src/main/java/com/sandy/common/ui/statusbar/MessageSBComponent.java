1c6970538ad18fa6f53fdd23823b6d6111c114ef
Sandeep Deb <deb.sandeep@gmail.com>
2016-04-01 17:20:02 +0530

    Refactored

---
+++ src/main/java/com/sandy/common/ui/statusbar/MessageSBComponent.java
@@ -0,0 +1,38 @@
+package com.sandy.common.ui.statusbar;
+
+import java.awt.BorderLayout ;
+
+import javax.swing.JLabel ;
+
+import org.apache.log4j.Logger ;
+
+import com.sandy.common.ui.statusbar.AbstractSBComponent ;
+import com.sandy.common.ui.statusbar.ClockSBComponent ;
+
+public class MessageSBComponent extends AbstractSBComponent {
+
+    private static final long serialVersionUID = -7087385729013001309L ;
+
+    public static Logger logger = Logger.getLogger( ClockSBComponent.class ) ;
+
+    private JLabel msgLabel = null;
+
+    @Override
+    public void initialize() {
+        setUpUI();
+    }
+
+    private void setUpUI() {
+        msgLabel = new JLabel() ;
+        clear() ;
+        super.add( this.msgLabel, BorderLayout.CENTER );
+    }
+    
+    public void logMessage( String text ) {
+        msgLabel.setText( text ) ;
+    }
+    
+    public void clear() {
+        msgLabel.setText( " " ) ;
+    }
+}
